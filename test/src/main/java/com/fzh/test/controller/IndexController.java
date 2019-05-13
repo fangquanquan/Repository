bd9300cdce5277115f2db309bbbcac23fa63d9a4
fangquanquan <891823874@qq.com>
2019-05-13 15:06:08 +0800

    create

---
+++ test/src/main/java/com/fzh/test/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.fzh.test.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @Value("${server.port}")
+    private int port;
+
+    @RequestMapping("/{value}")
+    public String getPort(@PathVariable("value") String value){
+        return "hello" + "port=" +port +"value="+value;
+    }
+}
